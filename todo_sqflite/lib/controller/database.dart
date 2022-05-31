c413efa8d0216a1753af97a94fbcf56f120889d9
neiloy <neiloyhassan1@gmail.com>
2022-06-01 02:21:29 +0600

    database logic

--- todo_sqflite/lib/controller/database.dart
+++ todo_sqflite/lib/controller/database.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
@@ -41,7 +40,92 @@ class NotesDatabase {
   }
 
   //Create Database if doesnt exist
-  Future _createDB(Database db, int version) async {}
+  Future _createDB(Database db, int version) async {
+    const idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+    const booleanType = 'BOOLEAN NOT NULL';
+    const intType = 'INTEGER NOT NULL';
+    const textType = 'TEXT NOT NULL';
+    await db.execute('''CREATE TABLE ${table}
+      (
+       ${NoteFields.id} $idType,
+       ${NoteFields.isImportant} $booleanType,
+       ${NoteFields.number} $intType,
+       ${NoteFields.title} $textType,
+       ${NoteFields.description} $textType,
+       ${NoteFields.time} $textType
+      )''');
+  }
+
+  Future<Note> create(Note note) async {
+    final db = await instance.database;
+
+    //Manual Code
+    // final json = note.toJson();
+    // final columns =
+    //     '${NoteFields.title}, ${NoteFields.description}, ${NoteFields.time}';
+    // final values =
+    //     '${json[NoteFields.title]}, ${json[NoteFields.description]}, ${json[NoteFields.time]}';
+    // final id = await db
+    //     .rawInsert('INSERT INTO table_name ($columns) VALUES ($values)');
+
+    //Autogenerate Code
+    final id = await db.insert(
+      table,
+      note.toJson(),
+    );
+    return note.copy(id: id);
+  }
+
+  Future<Note> readNote(int id) async {
+    final db = await instance.database;
+
+    final maps = await db.query(
+      table,
+      columns: NoteFields.values,
+      where: '${NoteFields.id} = ?',
+      //TODO: didn't understand the question mark and whereargs
+      whereArgs: [id],
+    );
+
+    if (maps.isNotEmpty) {
+      return Note.fromJson(maps.first); //why maps.first
+    } else {
+      throw Exception('ID $id not found');
+    }
+  }
+
+  Future<List<Note>> readAllNotes() async {
+    final db = await instance.database;
+
+    const orderBy = '${NoteFields.time} ASC';
+    // final result =
+    //     await db.rawQuery('SELECT * FROM $tableNotes ORDER BY $orderBy');
+
+    final result = await db.query(table, orderBy: orderBy);
+
+    return result.map((json) => Note.fromJson(json)).toList();
+  }
+
+  Future<int> update(Note note) async {
+    final db = await instance.database;
+
+    return db.update(
+      table,
+      note.toJson(),
+      where: '${NoteFields.id} = ?',
+      whereArgs: [note.id],
+    );
+  }
+
+  Future<int> delete(int id) async {
+    final db = await instance.database;
+
+    return await db.delete(
+      table,
+      where: '${NoteFields.id} = ?',
+      whereArgs: [id],
+    );
+  }
 
   Future close() async {
     //getting access to db using private constructor initialization
--- todo_sqflite/lib/models/notes.dart
+++ todo_sqflite/lib/models/notes.dart
@@ -1,12 +1,16 @@
-final String table = 'notes';
+const String table = 'notes';
 
 class NoteFields {
-  static final String id = '_id';
-  static final String isImportant = 'isImportant';
-  static final String number = 'number';
-  static final String title = 'title';
-  static final String description = 'description';
-  static final String time = 'time';
+  static final List<String> values = [
+    /// Add all fields
+    id, isImportant, number, title, description, time
+  ];
+  static const String id = '_id';
+  static const String isImportant = 'isImportant';
+  static const String number = 'number';
+  static const String title = 'title';
+  static const String description = 'description';
+  static const String time = 'time';
 }
 
 class Note {
@@ -25,4 +29,46 @@ class Note {
     required this.description,
     required this.createdTime,
   });
+
+  //Everything is nullable because we might not know which parameter to change
+  Note copy({
+    int? id,
+    bool? isImportant,
+    int? number,
+    String? title,
+    String? description,
+    DateTime? createdTime,
+  }) {
+    //if field is null return with the default value if not set new value and then return
+    return Note(
+      id: id ?? this.id,
+      isImportant: isImportant ?? this.isImportant,
+      number: number ?? this.number,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      createdTime: createdTime ?? this.createdTime,
+    );
+  }
+
+  //To read mapped data retrieved from database
+  //didn't fully understand this
+  static Note fromJson(Map<String, Object?> json) => Note(
+        id: json[NoteFields.id] as int?,
+        isImportant: json[NoteFields.isImportant] == 1,
+        number: json[NoteFields.number] as int,
+        title: json[NoteFields.title] as String,
+        description: json[NoteFields.description] as String,
+        createdTime: DateTime.parse(json[NoteFields.time] as String),
+      );
+
+  //converting Notes Object to Mapped Json Value pretaining to NoteFields
+  //TODO: Might need to change dynamic back to Object? if breaks
+  Map<String, dynamic> toJson() => {
+        NoteFields.id: id,
+        NoteFields.title: title,
+        NoteFields.isImportant: isImportant ? 1 : 0,
+        NoteFields.number: number,
+        NoteFields.description: description,
+        NoteFields.time: createdTime.toIso8601String(),
+      };
 }
